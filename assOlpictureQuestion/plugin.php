e66a95c1c6973d7370c4ef78e83267808c7f8974
kyro46 <cjobst@wifa.uni-leipzig.de>
2015-05-12 15:36:18 +0200

    Commit 2

--- assOlpictureQuestion/plugin.php
+++ assOlpictureQuestion/plugin.php
@@ -1,17 +1,16 @@
 <?php
  
 // alphanumerical ID of the plugin; never change this
-$id = "assPaintQuestion";
+$id = "assOlpictureQuestion";
  
 // code version; must be changed for all code changes
-$version = "1.1.10";
+$version = "0.0.1";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.4.0";
-$ilias_max_version = "5.0.999";
+$ilias_max_version = "4.4.999";
 
 $responsible = "Christoph Jobst";
 $responsible_mail = "christoph.jobst@llz.uni-halle.de";
-
 ?>
