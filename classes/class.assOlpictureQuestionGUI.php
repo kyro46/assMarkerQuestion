c84a765266023277939fdfee71af545eb197fe3f
Christoph Jobst <cjobst@wifa.uni-leipzig.de>
2016-05-03 20:37:10 +0200

    more work

--- classes/class.assOlpictureQuestionGUI.php
+++ classes/class.assOlpictureQuestionGUI.php
@@ -374,13 +374,14 @@ class assOlpictureQuestionGUI extends assQuestionGUI
 		
 		//openlayers preview output #############
 		
-		$tpl->addJavaScript($plugin->getDirectory().'/js/ol.js');
-		$tpl->addJavaScript($plugin->getDirectory().'/js/ol3-contextmenu.js');
-		$tpl->addJavaScript($plugin->getDirectory().'/js/contextmenu.js');
 		
+		$tpl->addJavaScript($plugin->getDirectory().'/js/ol-debug.js');
+		//$tpl->addJavaScript($plugin->getDirectory().'/js/bootstrap.js');
+		
+		//$tpl->addCss($plugin->getDirectory().'/css/bootstrap.css');
+		//$tpl->addCss($plugin->getDirectory().'/css/bootstrap-responsive.css');
+		$tpl->addCss($plugin->getDirectory().'/css/layout.css');
 		$tpl->addCss($plugin->getDirectory().'/css/ol.css');
-		$tpl->addCss($plugin->getDirectory().'/css/ol3-contextmenu.css');
-		$tpl->addCss($plugin->getDirectory().'/css/contextmenu.css');
 		
 		
 		$template = $plugin->getTemplate("output.html");
